2780f2010e57853f3ed327d036e27d2aeb7a2978
Connorj13 <connor_jeffers@yahoo.com>
2016-02-06 18:51:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ This bot demonstrates many of the core features of Botkit:
 
   Run your bot from the command line:
 
-    token=<MY TOKEN> node bot.js
+    token=<xoxb-20533606785-Bgfc3LBYoXvzoTNfcS8B8tVt> node bot.js
 
 # USE THE BOT:
 
